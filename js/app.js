8cb6d40a68b507520e1a56c381043ca90e753720
Fernando Mendonca de Almeida <fernando.m.al.91@gmail.com>
2014-12-18 00:57:15 -0300

    Adicionado funcionalidades do AngularJS com TypeScript

---
+++ js/app.js
@@ -0,0 +1,87 @@
+/// <reference path='../_all.ts' />
+/// <reference path='../_all.ts' />
+/// <reference path='../_all.ts' />
+/// <reference path='../_all.ts' />
+/// <reference path='../_all.ts' />
+var paginaPessoal;
+(function (paginaPessoal) {
+    'use strict';
+
+    var HomeCtrl = (function () {
+        function HomeCtrl($scope) {
+            this.$scope = $scope;
+            $scope.linksCurriculo = [
+                {
+                    url: 'http://lattes.cnpq.br/0307827644596863',
+                    label: 'Lattes'
+                },
+                {
+                    url: 'http://br.linkedin.com/pub/fernando-mendon%C3%A7a-de-almeida/21/3/52/',
+                    label: 'LinkedIn'
+                },
+                {
+                    url: 'https://github.com/fernandomalmeida/',
+                    label: 'GitHub'
+                }
+            ];
+
+            $scope.linksProjetos = [];
+            $scope.linksArtigos = [];
+        }
+        HomeCtrl.$inject = [
+            '$scope'
+        ];
+        return HomeCtrl;
+    })();
+    paginaPessoal.HomeCtrl = HomeCtrl;
+})(paginaPessoal || (paginaPessoal = {}));
+/// <reference path='../_all.ts' />
+var paginaPessoal;
+(function (paginaPessoal) {
+    'use strict';
+
+    var FooterCtrl = (function () {
+        function FooterCtrl($scope) {
+            this.$scope = $scope;
+            $scope.badges = [
+                {
+                    icon: "social-github",
+                    url: "https://github.com/fernandomalmeida/",
+                    label: "GitHub"
+                },
+                {
+                    icon: "social-linkedin",
+                    url: "http://br.linkedin.com/pub/fernando-mendon%C3%A7a-de-almeida/21/3/52/",
+                    label: "LinkedIn"
+                },
+                {
+                    icon: "mail",
+                    url: "mailto:fernando.m.al.91@gmail.com",
+                    label: "E-mail"
+                }
+            ];
+        }
+        FooterCtrl.$inject = [
+            '$scope'
+        ];
+        return FooterCtrl;
+    })();
+    paginaPessoal.FooterCtrl = FooterCtrl;
+})(paginaPessoal || (paginaPessoal = {}));
+/// <reference path="_all.ts"/>
+var paginaPessoal;
+(function (_paginaPessoal) {
+    'use strict';
+
+    var paginaPessoal = angular.module('paginaPessoal', []).controller('homeCtrl', _paginaPessoal.HomeCtrl).controller('footerCtrl', _paginaPessoal.FooterCtrl);
+})(paginaPessoal || (paginaPessoal = {}));
+/// <reference path="typings/jquery/jquery.d.ts"/>
+/// <reference path="typings/angularjs/angular.d.ts"/>
+/// <reference path="interfaces/ILinkOption.ts" />
+/// <reference path="interfaces/IIconLink.ts" />
+/// <reference path="interfaces/IHomeScope.ts" />
+/// <reference path="interfaces/IFooterScope.ts" />
+/// <reference path="controllers/HomeCtrl.ts" />
+/// <reference path="controllers/FooterCtrl.ts" />
+/// <reference path="app.ts" />
+//# sourceMappingURL=app.js.map
